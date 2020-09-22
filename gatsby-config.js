239878f291603c188e2f2d8940c905e0507a344c
Oliver Dumoulin <hi@odmln.com>
2020-09-21 23:32:14 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,17 +12,9 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-gtag`,
-      options: {
-        trackingId: `UA-127725448-1`,
-        head: false,
-        anonymize: true,
-      },
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-127725448-1",
-        head: false,
-        anonymize: true,
       },
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -32,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
       resolve: `gatsby-source-filesystem`,
       options: {
